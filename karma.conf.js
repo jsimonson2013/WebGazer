79cab35c857f97b63aa63c68de392f3c80d633fd
jsimonson <jsimonson2013@gmail.com>
2017-07-23 08:15:41 -0700

    Karma launches Firefox and runs spec tests.

--- karma.conf.js
+++ karma.conf.js
@@ -1,41 +1,57 @@
-// Karma configuration
-// Generated on Sun Jul 23 2017 05:33:35 GMT-0700 (US Mountain Standard Time)
+// Karma configuration from git repository x2es/boilerplate-karma-mocha-chai-requirejs
+// Generated on Mon Dec 02 2013 18:46:27 GMT+0400 (MSK)
+
+// uncomment this for redefine mp3-reporter sound files
+// ====================================================
+// var path = require('path');
+// var karmaSoundDir = path.join(__dirname, 'karma_sound');
+// ====================================================
 
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+    // base path, that will be used to resolve files and exclude
     basePath: '',
 
 
     // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'requirejs'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'test-main.js',
+      {pattern: 'node_modules/**/*.js', included: false},   // allow to load any *.js from node_modules by karma web-server
+
+      'test/test-main.js',
+      {pattern: 'test/spec/*.js', included: false},
+      {pattern: 'js/lib/*.js', included: false},
       {pattern: 'test/*.js', included: false}
     ],
 
 
     // list of files to exclude
     exclude: [
+
     ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
     // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    // reporters: ['progress', 'mp3', 'html'],
     reporters: ['progress'],
 
+    // uncomment this for redefine mp3-reporter sound files
+    // ====================================================
+    // mp3Reporter: {
+    //   red:        path.join(karmaSoundDir, 'red.mp3'),
+    //   green:      path.join(karmaSoundDir, 'green.mp3'),
+
+    //   fail:       path.join(karmaSoundDir, 'fail.mp3'),
+    //   error:      path.join(karmaSoundDir, 'error.mp3'),
+    //   success:    path.join(karmaSoundDir, 'success.mp3'),
+    //   disconnect: path.join(karmaSoundDir, 'success.mp3'),
+    // },
+    // ====================================================
 
     // web server port
     port: 9876,
@@ -54,17 +70,25 @@ module.exports = function(config) {
     autoWatch: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: [
+      'Firefox'
+    ],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
     // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
---
+++ test/spec/sample_spec.js
@@ -0,0 +1,27 @@
+define(['chai', 'sinon'], function(chai, sinon) {
+  var expect = chai.expect;
+
+  describe('bootstrap', function() {
+    it('chai should works', function() {
+      expect('a').to.equal('a');
+    });
+
+    it('sinon should works', function() {
+
+      function Obj() {}
+      Obj.prototype.method = function() {
+        return ('I am Obj#method()');
+      }
+
+      sinon.stub(Obj.prototype, 'method', function() {
+        return ('I am Sinon stub!');
+      });
+
+      var obj = new Obj();
+      expect(obj.method()).to.equal('I am Sinon stub!');
+
+      Obj.prototype.method.restore();
+      expect(obj.method()).to.equal('I am Obj#method()');
+    });
+  });
+});
